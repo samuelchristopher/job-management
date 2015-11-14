0457bf46d4f328ee2884c01ed3a2e17a5b88fcb0
Samuel Christopher <thesamchris@gmail.com>
2015-11-14 06:46:52 +0000

    Routing and rendering fix

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ var express    = require('express');
 var morgan     = require('morgan');
 var bodyParser = require('body-parser');
 var config     = require('./app/config');
+var path       = require('path');
 
 var app = express();
 
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, '/app/src/views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(express.static('build'));
+
+app.get('*', function(req, res) {
+  res.render('index', { a: "John" });
+});
 
 app.listen(config.port, function(err) {
   if (err) {
