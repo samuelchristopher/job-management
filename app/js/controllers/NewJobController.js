29cf55a5a48a4a9d40da8a786bcb64bd31c03085
Samuel Christopher <thesamchris@gmail.com>
2015-11-25 07:07:32 +0000

    Finished add job functionality

--- app/js/controllers/NewJobController.js
+++ app/js/controllers/NewJobController.js
@@ -1,12 +1,16 @@
 angular.module('jobManagement')
-  .controller('NewJobController', ['$scope', 'Auth', 'JobsService', NewJobController]);
+  .controller('NewJobController', ['$mdBottomSheet', 'FlashService', '$location', '$scope', 'Auth', 'JobsService', NewJobController]);
 
-function NewJobController($scope, Auth, JobsService) {
+function NewJobController($mdBottomSheet, FlashService, $location, $scope, Auth, JobsService) {
   $scope.header = 'New Job';
   var user = Auth.getUser();
   $scope.createdBy = user.password.email;
   $scope.newJobText = 'Create New Job';
   $scope.createNewJob = function() {
-
+    JobsService.addJob($scope.name, $scope.customerName, $scope.createdBy, $scope.desc, $scope.dueDate);
+    $mdBottomSheet.hide();
+    setTimeout(function() {
+      FlashService.toast('Added ' + $scope.name + '!', 'OK');
+    }, 99);
   };
 }
--- app/js/services/jobs.js
+++ app/js/services/jobs.js
@@ -8,11 +8,19 @@ function JobsService($firebaseArray) {
     return jobs;
   };
 
-  var addJob = function(name, customerName, desc) {
-
+  var addJob = function(name, customerName, createdBy, desc, dueDate) {
+    var newJob = {
+      name: name,
+      customerName: customerName,
+      createdBy: createdBy,
+      desc: desc,
+      dueDate: new Date(String(dueDate)).getTime() / 1000
+    };
+    jobs.$add(newJob);
   };
 
   return {
-    getJobs: getJobs
+    getJobs: getJobs,
+    addJob: addJob
   };
 }
