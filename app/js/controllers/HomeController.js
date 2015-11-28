ef8e9760835fa43b4f798f9d68f95222433c0163
Samuel Christopher <thesamchris@gmail.com>
2015-11-28 08:14:11 +0000

    Added completed and pending count for home page

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -1,8 +1,30 @@
 angular.module('jobManagement')
-  .controller('HomeController', ['currentAuth', '$rootScope', '$scope', '$location', 'Auth', 'FlashService', HomeController]);
+  .controller('HomeController', ['JobsService', 'currentAuth', '$rootScope', '$scope', '$location', 'Auth', 'FlashService', HomeController]);
 
-function HomeController(currentAuth, $rootScope, $scope, $location, Auth, FlashService) {
+function HomeController(JobsService, currentAuth, $rootScope, $scope, $location, Auth, FlashService) {
   $scope.user = currentAuth.password;
-  $scope.thing = 'It works!';
-  $scope.auth = Auth.getAuth();
+  var jobs = JobsService.getJobs();
+  jobs.$loaded().then(function() {
+    updateCounters(jobs);
+  });
+
+  jobs.$watch(function(e) {
+    updateCounters(jobs);
+  });
+
+  function updateCounters(jobs) {
+    var completedCount = 0;
+    var pendingCount = 0;
+    angular.forEach(jobs, function(job, key) {
+      if (job.completed) {
+        completedCount++;
+      } else {
+        pendingCount++;
+      }
+    });
+    $scope.pendingCount = pendingCount;
+    $scope.completedCount = completedCount;
+  }
+
+
 }
